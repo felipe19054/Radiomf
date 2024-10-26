1eaef279ccb4253251af0a466e8ef28267361a0b
manuel <manuelfelipe1713@gmail.com>
2024-10-25 22:53:13 -0500

    Cancion

--- src/com/cursos/radiomf/modelos/Cancion.java
+++ src/com/cursos/radiomf/modelos/Cancion.java
@@ -1,8 +1,31 @@
 package com.cursos.radiomf.modelos;
 
-public class Canciones {
+public class Cancion extends Audio{
     private String album;
     private String cantante;
     private String genero;
-    
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getCantante() {
+        return cantante;
+    }
+
+    public void setCantante(String cantante) {
+        this.cantante = cantante;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
 }
